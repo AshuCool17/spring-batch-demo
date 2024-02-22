a25d6ebf0e2a868742e93684db14fc216508fc15
Ashutosh <ashutosh.mahato@gmail.com>
2024-02-23 00:09:42 +0530

    Added fields for Customer

--- spring-batch-demo/src/main/java/com/continuecoding/entity/Customer.java
+++ spring-batch-demo/src/main/java/com/continuecoding/entity/Customer.java
@@ -3,10 +3,23 @@
  */
 package com.continuecoding.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
 /**
  * @author Ashutosh
  *
  */
+@Entity
+@Table
 public class Customer {
+	
+	private int id;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String email;
 
 }
