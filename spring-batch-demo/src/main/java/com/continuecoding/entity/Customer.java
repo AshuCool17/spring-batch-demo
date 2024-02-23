7cfefd0b01943b7cf4f357f5ac3e73ae8ef6a427
Ashutosh <ashutosh.mahato@gmail.com>
2024-02-23 22:35:25 +0530

    Added few more fields to Customer class

--- spring-batch-demo/src/main/java/com/continuecoding/entity/Customer.java
+++ spring-batch-demo/src/main/java/com/continuecoding/entity/Customer.java
@@ -3,8 +3,12 @@
  */
 package com.continuecoding.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 /**
  * @author Ashutosh
@@ -12,14 +16,27 @@ import jakarta.persistence.Table;
  */
 @Entity
 @Table
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
 	
+	@Id
 	private int id;
 	
+	@Column
 	private String firstName;
 	
+	@Column
 	private String lastName;
 	
+	@Column
 	private String email;
+	
+	@Column
+	private String gender;
+	
+	@Column
+	private String contactNumber;
+	
 
 }
