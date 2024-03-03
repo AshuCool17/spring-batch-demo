4ff766e497711ab23e2911a7fa62553dea0e45b9
Ashutosh <ashutosh.mahato@gmail.com>
2024-03-03 18:30:07 +0530

    created jobLauncher & job fields

--- spring-batch-demo/src/main/java/com/continuecoding/controller/JobController.java
+++ spring-batch-demo/src/main/java/com/continuecoding/controller/JobController.java
@@ -3,13 +3,21 @@
  */
 package com.continuecoding.controller;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Ashutosh
  *
  */
-@RestController
+@RestController("/jobs")
 public class JobController {
 
+	@Autowired
+	private JobLauncher jobLauncher;
+	
+	@Autowired
+	private Job job;
 }
