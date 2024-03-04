606490fc41bc31143f744818202cf63c0b4a904f
Ashutosh <ashutosh.mahato@gmail.com>
2024-03-04 23:45:55 +0530

    Added annotation

--- spring-batch-demo/src/main/java/com/continuecoding/controller/JobController.java
+++ spring-batch-demo/src/main/java/com/continuecoding/controller/JobController.java
@@ -6,6 +6,7 @@ package com.continuecoding.controller;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -21,6 +22,7 @@ public class JobController {
 	@Autowired
 	private Job job;
 	
+	@PostMapping
 	public void importCsvToDBJob() {
 		
 	}
