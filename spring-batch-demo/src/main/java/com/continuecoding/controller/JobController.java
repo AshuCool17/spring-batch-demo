2ed06385b9f3e073f6653bd322fe452d324fea3c
Ashutosh <ashutosh.mahato@gmail.com>
2024-03-04 23:47:41 +0530

    Added job parameter

--- spring-batch-demo/src/main/java/com/continuecoding/controller/JobController.java
+++ spring-batch-demo/src/main/java/com/continuecoding/controller/JobController.java
@@ -4,6 +4,9 @@
 package com.continuecoding.controller;
 
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,5 +28,7 @@ public class JobController {
 	@PostMapping
 	public void importCsvToDBJob() {
 		
+		JobParameters jobParameters = new JobParametersBuilder()
+				.addLong("startAt", System.currentTimeMillis());
 	}
 }
